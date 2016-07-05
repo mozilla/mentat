f7621712dfd624c9c157474c92e9b3f1a4a808f5
Richard Newman <rnewman@twinql.com>
2016-07-05 12:05:49 -0700

    Stub out Leiningen and npm project files.
    Signed-off-by: Richard Newman <rnewman@twinql.com>

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lib "0.1.0-SNAPSHOT"
+  :description "A persistent, embedded knowledge base inspired by Datomic and DataScript."
+  :url "https://github.com/mozilla/datomish"
+  :license {:name "Mozilla Public License Version 2.0"
+            :url "https://github.com/mozilla/datomish/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
