27d35f030dcf80f1c8bea358c124b5c6fed2c6cb
Richard Newman <rnewman@twinql.com>
2016-07-05 17:01:27 -0700

    Take dependencies on a few libraries.
    Signed-off-by: Richard Newman <rnewman@twinql.com>

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "https://github.com/mozilla/datomish"
   :license {:name "Mozilla Public License Version 2.0"
             :url "https://github.com/mozilla/datomish/blob/master/LICENSE"}
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :dependencies [[org.clojure/clojurescript "1.9.89"]])
