155f9b6d988034f5592ec6873f0e8693245a8c02
Richard Newman <rnewman@twinql.com>
2016-09-22 15:49:35 -0700

    Add Public Domain license to addon code.

--- addon/src/index.js
+++ addon/src/index.js
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
 var self = require("sdk/self");
 var buttons = require('sdk/ui/button/action');
 var tabs = require('sdk/tabs');
