b20e769abf955a53a1f10efb6968d2c6a0f87fa4
Richard Newman <rnewman@twinql.com>
2016-10-24 14:08:31 -0700

    Upgrade SQLite, bump version to 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datomish "0.1.2-SNAPSHOT"
+(defproject datomish "0.2.0-SNAPSHOT"
   :description "A persistent, embedded knowledge base inspired by Datomic and DataScript."
   :url "https://github.com/mozilla/datomish"
   :license {:name "Mozilla Public License Version 2.0"
