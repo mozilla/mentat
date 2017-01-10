a6545cc6c40b386735ffd5e2ef6cf7a4fafa3336
Brian Grinstead <briangrinstead@gmail.com>
2017-01-09 16:55:45 -0800

    update cli

--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,29 @@ extern crate clap;
 use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let matches = App::new("Datomish")
-    .arg_from_usage("--license 'display the license file'")
-                        .subcommand(SubCommand::with_name("serve")
-                                                .about("Starts a server")
-                                                .arg(Arg::with_name("debug")
-                                                    .long("debug")
-                                                    .help("Print debugging info"))
-                                                .arg(Arg::with_name("database")
-                                                     .short("d")
-                                                     .long("database")
-                                                     .value_name("FILE")
-                                                     .help("Path to the Datomish database to serve")
-                                                     .default_value("temp.db")
-                                                     .takes_value(true))
-                                                .arg(Arg::with_name("port")
-                                                     .short("p")
-                                                     .long("port")
-                                                     .value_name("INTEGER")
-                                                     .help("Port to serve from, i.e. `localhost:PORT`")
-                                                     .default_value("3333")
-                                                     .takes_value(true)))
-                        .get_matches();
+    let matches = App::new("Mentat")
+                    .subcommand(SubCommand::with_name("serve")
+                                            .about("Starts a server")
+                                            .arg(Arg::with_name("debug")
+                                                .long("debug")
+                                                .help("Print debugging info"))
+                                            .arg(Arg::with_name("database")
+                                                 .short("d")
+                                                 .long("database")
+                                                 .value_name("FILE")
+                                                 .help("Path to the Mentat database to serve")
+                                                 .default_value("temp.db")
+                                                 .takes_value(true))
+                                            .arg(Arg::with_name("port")
+                                                 .short("p")
+                                                 .long("port")
+                                                 .value_name("INTEGER")
+                                                 .help("Port to serve from, i.e. `localhost:PORT`")
+                                                 .default_value("3333")
+                                                 .takes_value(true)))
+                    .get_matches();
 
-    // You can get the independent subcommand matches (which function exactly like App matches)
     if let Some(ref matches) = matches.subcommand_matches("serve") {
-        // Safe to use unwrap() because of the required() option
         let debug = matches.is_present("debug");
         println!("This doesn't work yet, but it will eventually serve the following database: {} on port: {}.  Debugging={}",
                     matches.value_of("database").unwrap(),
