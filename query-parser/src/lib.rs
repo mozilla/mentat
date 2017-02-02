3506c564c078f95521c6892595c2502a0c166679
Richard Newman <rnewman@twinql.com>
2017-02-02 07:52:59 -0800

    Remove allow(unused_imports): no warning is printed.

--- query-parser/src/lib.rs
+++ query-parser/src/lib.rs
@@ -8,7 +8,6 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#![allow(unused_imports)]
 #[macro_use]
 extern crate parser_utils;
 
