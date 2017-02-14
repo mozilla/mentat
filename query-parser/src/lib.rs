896d7f8f88bc65a5c922dad967768c9d9cf2af76
Victor Porof <victor.porof@gmail.com>
2017-02-14 16:43:32 +0100

    Add a span component to edn::Value, r=ncalexan
    Signed-off-by: Victor Porof <victor.porof@gmail.com>

--- query-parser/src/lib.rs
+++ query-parser/src/lib.rs
@@ -8,6 +8,8 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+#![allow(unused_imports)]
+
 #[macro_use]
 extern crate matches;
 
