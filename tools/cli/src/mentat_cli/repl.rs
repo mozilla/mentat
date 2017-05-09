9b30a2c0a73cac2c25e184b5136b4f939c74d4da
Emily Toop <etoop@mozilla.com>
2017-05-09 10:54:11 +0100

    Create mentat command line. * Create tools directory containing new crate for mentat_cli. * Add simple cli with mentat prompt.

---
+++ tools/cli/src/mentat_cli/repl.rs
@@ -0,0 +1,75 @@
+// Copyright 2017 Mozilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+use input::{InputReader};
+use input::InputResult::{Command, Empty, More, Eof};
+
+/// Starting prompt
+const DEFAULT_PROMPT: &'static str = "mentat=> ";
+/// Prompt when further input is being read
+const MORE_PROMPT: &'static str = "mentat.> ";
+/// Prompt when a `.block` command is in effect
+const BLOCK_PROMPT: &'static str = "mentat+> ";
+
+/// Executes input and maintains state of persistent items.
+pub struct Repl {
+}
+
+impl Repl {
+    /// Constructs a new `Repl`.
+    pub fn new() -> Repl {
+        Repl{}
+    }
+
+
+    /// Runs the REPL interactively.
+    pub fn run(&mut self) {
+        let mut more = false;
+        let mut input = InputReader::new();
+
+        loop {
+            let res = input.read_input(if more { MORE_PROMPT } else { DEFAULT_PROMPT });
+        //     let res = if self.read_block {
+        //         self.read_block = false;
+        //         input.read_block_input(BLOCK_PROMPT)
+        //     } else {
+        //         input.read_input(if more { MORE_PROMPT } else { DEFAULT_PROMPT })
+        //     };
+
+            match res {
+                Command(name, args) => {
+                    debug!("read command: {} {:?}", name, args);
+
+                    more = false;
+                    self.handle_command(name, args);
+                },
+                Empty => (),
+                More => { more = true; },
+                Eof => {
+                    if input.is_tty() {
+                        println!("");
+                    }
+                    break;
+                }
+            };
+        }
+    }
+
+    /// Runs a single command input.
+    fn handle_command(&mut self, cmd: String, args: Option<String>) {
+        println!("{:?} {:?}", cmd, args);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
