55588209c296272ae30cc8be96c0aa179337c2ef
Emily Toop <etoop@mozilla.com>
2017-11-21 16:56:16 +0000

    CLI (#493)
    * Create mentat command line.
    * Create tools directory containing new crate for mentat_cli.
    * Add simple cli with mentat prompt.
    
    * Remove rustc-serialize dependency
    
    * Open DB inside CLI (#452) (#463)
    
    * Open named database OR default to in memory database if no name provided
    
    Rearrange workspace to allow import of mentat crate in cli crate
    
    Create store object inside repl when started for connecting to mentat
    
    Use provided DB name to open connection in store
    
    Accept DB name as command line arg.
    
    Open on CLI start
    
    Implement '.open' command to open desired DB from inside CLI
    
    * Implement Close command to close current DB.
    * Closes existing open db and opens new in memory db
    
    * Review comment: Use `combine` to parse arguments.
    
    Move over to using Result rather than enums with err
    
    * Accept and parse EDN Query and Transact commands (#453) (#465)
    
    * Parse query and transact commands
    
    * Implement is_complete for transactions and queries
    
    * Improve query parser. Am still not happy with it though.
    
    There must be some way that I can retain the eof() after the `then` that means I don't have to move the skip on spaces and eof
    
    Make in process command storing clearer.
    
    Add comments around in process commands.
    Add alternative commands for transact/t and query/q
    
    * Address review comments r=nalexander.
    
    * Bump rust version number.
    * Use `bail` when throwing errors.
    * Improve edn parser.
    * Remove references to unused `more` flag.
    * Improve naming of query and transact commands.
    
    * Send queries and transactions to mentat and output the results (#466)
    
    * Send queries and transactions to mentat and output the results
    
    move outputting query and transaction results out of store and into repl
    
    * Add query and transact commands to help
    
    * Execute queries and transacts passed in at startup
    
    * Address review comments =nalexander.
    
    * Bump rust version number.
    * Use `bail` when throwing errors.
    * Improve edn parser.
    * Remove references to unused `more` flag.
    * Improve naming of query and transact commands.
    
    * Execute command line args in order
    
    * Addressing rebase issues
    
    * Exit CLI (#457) (#484) r-rnewman
    
    * Implement exit command for cli tool
    
    * Address review comments r=rnewman
    
    * Include exit commands in help
    
    * Show schema of current DB (#487)
    
    * Fixing rebase issues
    
    * addressing nit
    
    * Match updated dependencies on CLI crate and remove unused import

---
+++ tools/cli/src/mentat_cli/lib.rs
@@ -0,0 +1,121 @@
+// Copyright 2017 Mozilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#![crate_name = "mentat_cli"]
+
+#[macro_use] extern crate log;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate error_chain;
+
+extern crate combine;
+extern crate env_logger;
+extern crate getopts;
+extern crate linefeed;
+extern crate rusqlite;
+
+extern crate mentat;
+extern crate edn;
+extern crate mentat_query;
+extern crate mentat_core;
+extern crate mentat_db;
+
+use getopts::Options;
+
+pub mod command_parser;
+pub mod store;
+pub mod input;
+pub mod repl;
+pub mod errors;
+
+pub fn run() -> i32 {
+    env_logger::init().unwrap();
+
+    let args = std::env::args().collect::<Vec<_>>();
+    let mut opts = Options::new();
+
+    opts.optopt("d", "", "The path to a database to open", "DATABASE");
+    opts.optflag("h", "help", "Print this help message and exit");
+    opts.optmulti("q", "query", "Execute a query on startup. Queries are executed after any transacts.", "QUERY");
+    opts.optmulti("t", "transact", "Execute a transact on startup. Transacts are executed before queries.", "TRANSACT");
+    opts.optflag("v", "version", "Print version and exit");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => {
+            println!("{}: {}", args[0], e);
+            return 1;
+        }
+    };
+
+    if matches.opt_present("version") {
+        print_version();
+        return 0;
+    }
+
+    if matches.opt_present("help") {
+        print_usage(&args[0], &opts);
+        return 0;
+    }
+
+    let mut last_arg: Option<&str> = None;
+    let cmds:Vec<command_parser::Command> = args.iter().filter_map(|arg| {
+        match last_arg {
+            Some("-d") => {
+                last_arg = None;
+                Some(command_parser::Command::Open(arg.clone()))
+            },
+            Some("-q") => {
+                last_arg = None;
+                Some(command_parser::Command::Query(arg.clone()))
+            },
+            Some("-t") => {
+                last_arg = None;
+                Some(command_parser::Command::Transact(arg.clone()))
+            },
+            Some(_) |
+            None => {
+                last_arg = Some(&arg);
+                None
+            },
+        } 
+    }).collect();
+
+    let repl = repl::Repl::new();
+    if repl.is_ok() {
+        repl.unwrap().run(Some(cmds));
+
+    } else {
+        println!("{}", repl.err().unwrap());
+    }
+
+    0
+}
+
+/// Returns a version string.
+pub fn version() -> &'static str {
+    env!("CARGO_PKG_VERSION")
+}
+
+fn print_usage(arg0: &str, opts: &Options) {
+    print!("{}", opts.usage(&format!(
+        "Usage: {} [OPTIONS] [FILE]", arg0)));
+}
+
+fn print_version() {
+    println!("mentat {}", version());
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
---
+++ tools/cli/src/mentat_cli/repl.rs
@@ -0,0 +1,226 @@
+// Copyright 2017 Mozilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+use std::collections::HashMap;  
+use std::process;
+
+use mentat::query::QueryResults;
+use mentat_core::TypedValue;
+
+use command_parser::{
+    Command, 
+    HELP_COMMAND, 
+    OPEN_COMMAND,
+    LONG_QUERY_COMMAND,
+    SHORT_QUERY_COMMAND,
+    SCHEMA_COMMAND,
+    LONG_TRANSACT_COMMAND,
+    SHORT_TRANSACT_COMMAND,
+    LONG_EXIT_COMMAND,
+    SHORT_EXIT_COMMAND,
+};
+use input::InputReader;
+use input::InputResult::{
+    MetaCommand, 
+    Empty, 
+    More, 
+    Eof
+};
+use store::{ 
+    Store,
+    db_output_name
+};
+
+lazy_static! {
+    static ref COMMAND_HELP: HashMap<&'static str, &'static str> = {
+        let mut map = HashMap::new();
+        map.insert(LONG_EXIT_COMMAND, "Close the current database and exit the REPL.");
+        map.insert(SHORT_EXIT_COMMAND, "Shortcut for `.exit`. Close the current database and exit the REPL.");
+        map.insert(HELP_COMMAND, "Show help for commands.");
+        map.insert(OPEN_COMMAND, "Open a database at path.");
+        map.insert(LONG_QUERY_COMMAND, "Execute a query against the current open database.");
+        map.insert(SHORT_QUERY_COMMAND, "Shortcut for `.query`. Execute a query against the current open database.");
+        map.insert(SCHEMA_COMMAND, "Output the schema for the current open database.");
+        map.insert(LONG_TRANSACT_COMMAND, "Execute a transact against the current open database.");
+        map.insert(SHORT_TRANSACT_COMMAND, "Shortcut for `.transact`. Execute a transact against the current open database.");
+        map
+    };
+}
+
+/// Executes input and maintains state of persistent items.
+pub struct Repl {
+    store: Store
+}
+
+impl Repl {
+    /// Constructs a new `Repl`.
+    pub fn new() -> Result<Repl, String> {
+        let store = Store::new(None).map_err(|e| e.to_string())?;
+        Ok(Repl{
+            store: store,
+        })
+    }
+
+    /// Runs the REPL interactively.
+    pub fn run(&mut self, startup_commands: Option<Vec<Command>>) {
+        let mut input = InputReader::new();
+
+        if let Some(cmds) = startup_commands {
+            for command in cmds.iter() {
+                println!("{}", command.output());
+                self.handle_command(command.clone());
+            }
+        }
+
+        loop {
+            let res = input.read_input();
+
+            match res {
+                Ok(MetaCommand(cmd)) => {
+                    debug!("read command: {:?}", cmd);
+                    self.handle_command(cmd);
+                },
+                Ok(Empty) |
+                Ok(More) => (),
+                Ok(Eof) => {
+                    if input.is_tty() {
+                        println!("");
+                    }
+                    break;
+                },
+                Err(e) => println!("{}", e.to_string()),
+            }
+        }
+    }
+
+    /// Runs a single command input.
+    fn handle_command(&mut self, cmd: Command) {
+        match cmd {
+            Command::Help(args) => self.help_command(args),
+            Command::Open(db) => {
+                match self.store.open(Some(db.clone())) {
+                    Ok(_) => println!("Database {:?} opened", db_output_name(&db)),
+                    Err(e) => println!("{}", e.to_string())
+                };
+            },
+            Command::Close => self.close(),
+            Command::Query(query) => self.execute_query(query),
+            Command::Schema => {
+                let edn = self.store.fetch_schema();
+                match edn.to_pretty(120) {
+                    Ok(s) => println!("{}", s),
+                    Err(e) => println!("{}", e)
+                };
+                
+            }
+            Command::Transact(transaction) => self.execute_transact(transaction),
+            Command::Exit => {
+                self.close();
+                println!("Exiting...");
+                process::exit(0);
+            }
+        }
+    }
+
+    fn close(&mut self) {
+        let old_db_name = self.store.db_name.clone();
+        match self.store.close() {
+            Ok(_) => println!("Database {:?} closed", db_output_name(&old_db_name)),
+            Err(e) => println!("{}", e)
+        };
+    }
+
+    fn help_command(&self, args: Vec<String>) {
+        if args.is_empty() {
+            for (cmd, msg) in COMMAND_HELP.iter() {
+                println!(".{} - {}", cmd, msg);
+            }
+        } else {
+            for mut arg in args {
+                if arg.chars().nth(0).unwrap() == '.' { 
+                    arg.remove(0);
+                }
+                let msg = COMMAND_HELP.get(arg.as_str());
+                if msg.is_some() {
+                    println!(".{} - {}", arg, msg.unwrap());
+                } else {
+                    println!("Unrecognised command {}", arg);
+                }
+            }
+        }
+    }
+
+    pub fn execute_query(&self, query: String) {
+        let results = match self.store.query(query){
+            Result::Ok(vals) => {
+                vals
+            },
+            Result::Err(err) => return println!("{:?}.", err),
+        };
+
+        if results.is_empty() {
+            println!("No results found.")
+        }
+        
+        let mut output:String = String::new();
+        match results {
+            QueryResults::Scalar(Some(val)) => { 
+                output.push_str(&self.typed_value_as_string(val) ); 
+            },
+            QueryResults::Tuple(Some(vals)) => { 
+                for val in vals {
+                    output.push_str(&format!("{}\t", self.typed_value_as_string(val)));
+                }
+            },
+            QueryResults::Coll(vv) => { 
+                for val in vv {
+                    output.push_str(&format!("{}\n", self.typed_value_as_string(val)));
+                }
+            },
+            QueryResults::Rel(vvv) => { 
+                for vv in vvv {
+                    for v in vv {
+                        output.push_str(&format!("{}\t", self.typed_value_as_string(v)));
+                    }
+                    output.push_str("\n");
+                }
+            },
+            _ => output.push_str(&format!("No results found."))
+        }
+        println!("\n{}", output);
+    }
+
+    pub fn execute_transact(&mut self, transaction: String) {
+        match self.store.transact(transaction) {
+            Result::Ok(report) => println!("{:?}", report),
+            Result::Err(err) => println!("{:?}.", err),
+        }
+    }
+
+    fn typed_value_as_string(&self, value: TypedValue) -> String {
+        match value {
+            TypedValue::Boolean(b) => if b { "true".to_string() } else { "false".to_string() },
+            TypedValue::Double(d) => format!("{}", d),
+            TypedValue::Instant(i) => format!("{}", i),
+            TypedValue::Keyword(k) => format!("{}", k),
+            TypedValue::Long(l) => format!("{}", l),
+            TypedValue::Ref(r) => format!("{}", r),
+            TypedValue::String(s) => format!("{:?}", s.to_string()),
+            TypedValue::Uuid(u) => format!("{}", u),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
