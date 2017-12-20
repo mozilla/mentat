f2a53c171ebe8ff8f3819c9ca54fb4cfde06413b
Grisha Kruglov <gkruglov@mozilla.com>
2017-12-20 01:51:25 -0500

    Expand comment

--- db/src/db.rs
+++ db/src/db.rs
@@ -69,9 +69,12 @@ pub fn new_connection<T>(uri: T) -> rusqlite::Result<rusqlite::Connection> where
         _ => rusqlite::Connection::open(uri)?,
     };
 
-    /// temp_store=2 is present to force SQLite to store temp files in memory.
+    /// See https://github.com/mozilla/mentat/issues/505 for details on temp_store
+    /// pragma and how it might interact together with consumers such as Firefox.
+    /// temp_store=2 is currently present to force SQLite to store temp files in memory.
     /// Some of the platforms we support do not have a tmp partition (e.g. Android)
-    /// necessary to store temp files on disk.
+    /// necessary to store temp files on disk. Ideally, consumers should be able to
+    /// override this behaviour (see issue 505).
     conn.execute_batch("
         PRAGMA page_size=32768;
         PRAGMA journal_mode=wal;
