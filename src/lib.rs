2ab481f83ec0fe7b91a198f8185f8521d588a205
Nick Alexander <nalexander@mozilla.com>
2018-07-03 14:18:06 -0700

    Part 2: Expose time related things at top-level.
    Perhaps we actually want to subdivide the top-level namespace so that
    there is a `mentat::time` module, but I'd prefer to make part of the
    process of fixing the public API as we get ready to christen version
    1.0.

--- src/lib.rs
+++ src/lib.rs
@@ -34,18 +34,20 @@ extern crate mentat_tolstoy;
 
 pub use mentat_core::{
     Attribute,
-    Entid,
-    DateTime,
-    HasSchema,
-    KnownEntid,
-    Keyword,
-    Schema,
     Binding,
+    DateTime,
+    Entid,
+    HasSchema,
+    Keyword,
+    KnownEntid,
+    Schema,
+    StructuredMap,
     TxReport,
     TypedValue,
-    Uuid,
     Utc,
+    Uuid,
     ValueType,
+    now,
 };
 
 pub use mentat_query::{
@@ -109,7 +111,13 @@ pub use errors::{
     Result,
 };
 
-pub use edn::ParseError;
+pub use edn::{
+    FromMicros,
+    FromMillis,
+    ParseError,
+    ToMicros,
+    ToMillis,
+};
 pub use mentat_db::DbError;
 pub use mentat_query_algebrizer::AlgebrizerError;
 pub use mentat_query_projector::ProjectorError;
