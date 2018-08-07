120326ae75584797b108169388a549d7574f1675
Emily Toop <etoop@mozilla.com>
2018-08-07 16:37:14 +0100

    Add iOS build task

--- scripts/ios_build.sh
+++ scripts/ios_build.sh
@@ -2,6 +2,5 @@ cd ffi
 cargo lipo --release
 cd ..
 mkdir -p sdks/swift/Mentat/External-Dependencies/
-pwd
-ls target/universal/release/
+ls -la
 cp target/universal/release/libmentat_ffi.a sdks/swift/Mentat/External-Dependencies/
