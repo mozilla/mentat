8948c8436a1aa7d84682a342beb88c09f7a4dae0
Emily Toop <etoop@mozilla.com>
2018-08-23 15:38:22 +0100

    Remove unused script

--- scripts/cargo-doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-    cargo doc &&
-    echo "<meta http-equiv=refresh content=0;url=mentat/index.html>" > target/doc/index.html &&
-    git clone https://github.com/davisp/ghp-import.git &&
-    ./ghp-import/ghp_import.py -n -p -f -r https://"$GH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG.git" target/doc
-fi
